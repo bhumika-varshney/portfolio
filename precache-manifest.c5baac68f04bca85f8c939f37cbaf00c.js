602002561ebb528f0d9b79e2edbf01ef05dbcf84
ayush9540 <018.ayush@gmail.com>
2021-06-21 10:00:54 +0530

    Updates

---
+++ precache-manifest.c5baac68f04bca85f8c939f37cbaf00c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43f506a8885618624bfbb96041ef42c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "edce92d18f6ccf22e737",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0fee88e39cf58bfc46ff",
+    "url": "/portfolio/static/css/main.31eb878e.chunk.css"
+  },
+  {
+    "revision": "edce92d18f6ccf22e737",
+    "url": "/portfolio/static/js/2.65665ff5.chunk.js"
+  },
+  {
+    "revision": "02f6b8ac52d08931dc74bcb1a5c762ae",
+    "url": "/portfolio/static/js/2.65665ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fee88e39cf58bfc46ff",
+    "url": "/portfolio/static/js/main.2a72565d.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  },
+  {
+    "revision": "dbba67a3415c2b2669646a3cdab2462b",
+    "url": "/portfolio/static/media/BG_Dark.dbba67a3.jpg"
+  },
+  {
+    "revision": "10fafba00d988539d637f07da5237ed4",
+    "url": "/portfolio/static/media/BG_Light.10fafba0.jpg"
+  }
+]);
